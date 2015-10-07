ae459e0e03dc6337e6d7a77775497f2658918f80
Josefine Rutgerson <josefinerutgerson@gmail.com>
2015-10-07 18:47:29 +0000

    More work on form validation

--- js/form.js
+++ js/form.js
@@ -1,38 +1,64 @@
 $(document).ready(function() {
-	$('#contact_name').on('input', function() {
-	var input=$(this);
-	var is_name=input.val();
-	if(is_name){input.removeClass("invalid").addClass("valid");}
-	else{input.removeClass("valid").addClass("invalid");}
-});
+    $('#contact_name').on('input', function() {
+        var input = $(this);
+        var is_name = input.val();
+        if (is_name) {
+            input.removeClass("invalid").addClass("valid");
+            $("#nameDetails").text("");
+            
+        }
+        else {
+            input.removeClass("valid").addClass("invalid");
+            $("#nameDetails").text("This field is required");
+        }
+    });
 
-// Email must be an email
-$('#contact_email').on('input', function() {
-	var input=$(this);
-	var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-	var is_email=re.test(input.val());
-	if(is_email){input.removeClass("invalid").addClass("valid");}
-	else{input.removeClass("valid").addClass("invalid");}
-});
+    // Email must be an email
+    $('#contact_email').on('input', function() {
+        var input = $(this);
+        var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+        var is_email = re.test(input.val());
+        if (is_email) {
+            input.removeClass("invalid").addClass("valid");
+             $("#emailDetails").text("");
+            
+            
+        }
+        else {
+            input.removeClass("valid").addClass("invalid");
+            $("#emailDetails").text("A valid email address is required");
+        }
+    });
 
-// Website must be a website
-$('#contact_tel').on('input', function() {
-    var input=$(this);
-	var re = "\\b\\d{3,14}\\b"; // regex for telephone number that are 3 to 14 digits long
-	var is_tel=re.test(input.val());
-	if(is_tel){input.removeClass("invalid").addClass("valid");}
-	else{input.removeClass("valid").addClass("invalid");}
-});
 
-// Message can't be blank
-$('#contact_message').keyup(function(event) {
-	var input=$(this);
-	var message=$(this).val();
-	console.log(message);
-	if(message){input.removeClass("invalid").addClass("valid");}
-	else{input.removeClass("valid").addClass("invalid");}	
-});
+    $('#contact_tel').on('input', function() {
+        var input = $(this);
+        var re = /^\d{3,14}$/; // regex for telephone number that are 3 to 14 digits long
+        var is_tel = re.test(input.val());
+        if (is_tel) {
+            input.removeClass("invalid").addClass("valid");
+             $("#telDetails").text("");
+        }
+        else {
+            input.removeClass("valid").addClass("invalid");
+            $("#telDetails").text("A valid number is required");
+        }
+    });
 
-$("#contact_submit button").click(function(event){
-}); 
+    // Message can't be blank
+    $('#contact_message').keyup(function(event) {
+        var input = $(this);
+        var message = $(this).val();
+        console.log(message);
+        if (message) {
+            input.removeClass("invalid").addClass("valid");
+            $("#messageDetails").text("");
+        }
+        else {
+            input.removeClass("valid").addClass("invalid");
+            $("#messageDetails").text("This field is required");
+        }
+    });
+
+    $("#contact_submit button").click(function(event) {});
 });
