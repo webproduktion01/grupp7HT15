ea3813f0dece1b16fd2029e6733f0148b7da92f2
webproduktion01 <oracleliaprojekt@gmail.com>
2015-10-07 10:03:13 +0000

    Added calendar. Need to tweak it

--- js/mobile.js
+++ js/mobile.js
@@ -1,30 +1,3 @@
-/* Code from https://css-tricks.com/convert-menu-to-dropdown/ */
-/*
-// Create the dropdown base
-$("<select />").appendTo("nav");
-
-// Create default option "Go to..."
-$("<option />", {
-   "selected": "selected",
-   "value"   : "",
-   "text"    : "Go to..."
-}).appendTo("nav select");
-
-// Populate dropdown with menu items
-$("nav a").each(function() {
- var el = $(this);
- $("<option />", {
-     "value"   : el.attr("href"),
-     "text"    : el.text()
- }).appendTo("nav select");
+$(document).ready(function() {
+    $("#calendar").weekCalendar();
 });
-
-$("nav select").change(function() {
-  window.location = $(this).find("option:selected").val();
-});
-
-$("nav select").click(function() {
-  window.location = $(this).find("option:selected").val();  
-});
-console.log("Test");
-*/
