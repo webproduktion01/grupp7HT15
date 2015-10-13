903f2b0058350f635ac259ad031f85933f14d720
webproduktion01 <oracleliaprojekt@gmail.com>
2015-10-13 06:53:36 +0000

    Remade jquery slide to object and applied it to teachers as well

--- js/mobile.js
+++ js/mobile.js
@@ -1,47 +1,105 @@
 $(document).ready(function() {
-    //Preventing any new animations to start while one is still in progress
-    var animated = false;
 
-    $(".vehicle").click(function() {
-        //console.log(this);
-        var index = $(this).index();
-        //Entities i use in this listener
-        var current = $(this);
-        var parent = $(this).parent();
-        //Speed for animation
-        var animationTime = 1000;
-        //console.log("IS something animated?: " + animated);
-        if (!animated) {
-            //It be animated if it isn't animated
-            animated = true;
-            //Emptying entity queues. Should probably do this at end of animation o.o
-            current.dequeue();
-            parent.dequeue();
-            //console.log(index);
-            //Using index to differentiate which vehicles are visible
-            if (index === 1) {
-                //Left animation, dependant on margin-left of current vehicle
-                current.animate({
-                    "margin-left": "-25%"
-                }, animationTime, animationEnd).queue(function() {
-                    current.prev().appendTo(current.parent());
-                    current.css("margin-left", 0);
-                });
-            }
-            else if (index === 2) {
-                //Right animation. Wholly dependant on parent container
-                parent.animate({
-                    "padding-left": "50%"
-                }, animationTime, animationEnd).queue(function() {
-                    current.next().prependTo(parent);
-                    parent.css("padding-left", 0);
-                });
-            }
-        }
-        //Function that runs att end of animation, makes sure animation is set to false again.
-        function animationEnd() {
-            animated = false;
-            //console.log("Animation has ended");
-        }
-    });
+    var slide = new slideShow(".vehicle", 1000, 2);
+    slide.createSlide();
+    var slide2 = new slideShow(".teacher",1000,2);
+
 });
+/* 	
+	Minimum itemcount needs to be 4 minimum
+	containerContainer needs to be wide enough to fit all items on the x-axis:
+    Example containerContainer size:  4 items at 50% each = 200%
+ 	Viewport maximum size is atleast (number of items - 2) 
+	Viewport minimum size is atleast 2 items wide
+*/
+
+function slideShow(tClass, animationSpeed, viewableItems) {
+    this.tClass = tClass;
+    this.animationSpeed = animationSpeed;
+    this.viewableItems = viewableItems;
+    //console.log(tClass);
+    //console.log($(tClass));
+    var items = $(tClass).length;
+    //console.log(items);
+
+    // calculate itemwidth = 100/ viewableitems = item width size
+    // containerContainer size = (item width * items);
+    // viewport = 100%
+    // 
+    //Setting components size
+    // 100px
+    var itemWidth;
+    //200px
+    var viewport = 100;
+    //100px
+    itemWidth = viewport / viewableItems;
+    //400px
+    var containerWidth = (items * itemWidth);
+    itemWidth = itemWidth/2;
+    $(tClass).css({
+        "min-width": itemWidth + "%",
+        "max-width": itemWidth + "%",
+        "width": itemWidth + "%",
+        "display":"block",
+    	"float":"left",
+    	"background-color":"yellow",
+    	"margin":0
+    });
+    //Setting container width
+    $(tClass).parent().css({
+        "min-width": containerWidth + "%",
+        "max-width": containerWidth + "%",
+        "width": containerWidth + "%",
+    	"margin":0,
+        "margin-left":"-"+itemWidth*2 + "%",
+    	"display":"block",
+        "background-color":"green"
+    });
+    $(tClass).parent().parent().css({
+       "min-width": "100%",
+       "max-width": "100%",
+       "width": "100%",
+    	"overflow":"hidden",
+    	"margin":0
+    });
+	/* Creating the actuall slide*/
+    this.createSlide = function() {
+    	var animated = false;        
+        $(tClass).click(function() {
+            console.log($(tClass));
+             var current = $(this);
+            var index = current.index();
+            console.log("index: "+index);
+            var parent = current.parent();
+            console.log("Är något animaterat?: " + animated);
+            if (!animated) {
+                animated = true;
+                current.dequeue();
+                parent.dequeue();
+                if (index === 1) {
+                    //Left animation
+                    current.animate({
+                        "margin-left": "-" + itemWidth + "%"
+                    }, animationSpeed, animationEnd).queue(function() {
+                        current.prev().appendTo(current.parent());
+                        current.css("margin-left", 0);
+                    });
+                }
+                else if (index === 2) {
+                    //Right animation
+                    parent.animate({
+                        "padding-left": itemWidth + "%"
+                    }, animationSpeed, animationEnd).queue(function() {
+                        current.next().prependTo(parent);
+                        parent.css("padding-left", 0);
+                    });
+                }
+            }
+
+            function animationEnd() {
+                animated = false;
+                console.log("Animation has ended");
+            }
+        });
+    };
+}
